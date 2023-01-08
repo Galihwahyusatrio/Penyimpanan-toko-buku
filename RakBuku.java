583f0dae728a31ca329924257ff820159dccc9d9
Galihwahyusatrio <101864842+Galihwahyusatrio@users.noreply.github.com>
2023-01-08 11:44:05 +0700

    Add files via upload

---
+++ RakBuku.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public abstract class RakBuku implements Penyimpanan {
+    @Override
+    public abstract void setData();
+    @Override
+    public abstract void tampil();
+
+}
+
+
